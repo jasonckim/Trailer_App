5c63a1dfd8a57ee8e9669b9a3e981f89b6b67c1b
Jason <jasonckim10@gmail.com>
2014-05-08 17:45:02 -0700

    another try again

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups(assets: %w(development test production)))
 
 module TrailerApp
   class Application < Rails::Application
